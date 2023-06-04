ff8ba184744a5560eaed37505d5bbd3ed4771aaf
utkarshnath <nath.utkarsh1@gmail.com>
2023-06-04 02:20:45 -0400

    working on all comments ui

--- src/components/Post/CommentItem.tsx
+++ src/components/Post/CommentItem.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableWithoutFeedback, ImageSourcePropType } from 'react-native';
+import {themeFontFamily, themefonts,themeColor} from '../../constants/theme';
 import Icon from 'react-native-vector-icons/Entypo';
 import ADIcon from 'react-native-vector-icons/AntDesign';
 import ProfilePicture from '../ProfilePicture';
@@ -23,24 +24,31 @@ const CommentItem: React.FC<PropsType> = ({comment}) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.left}>
-        <ProfilePicture uri={shikha} size={30} borderWidth={2}/>
-        <Text style={styles.name}>{comment.username}</Text>
-        <Text>Date</Text>
+      <View style={styles.topcontainer}>
+        <View style={styles.left}>
+          <ProfilePicture uri={shikha} size={30} borderWidth={2}/>
+        </View>
+        <View style={styles.right}>
+          <View style={styles.top}>
+            <Text style={styles.name}>{comment.username}</Text>
+            <View>
+              <Text style={styles.date}>Date</Text>
+            </View>
+          </View>
+          <View style={{marginRight: 5, flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={styles.comment}>{comment.text}</Text>
+          </View>
+        </View>
       </View>
-
-      <View style={styles.right}>
-        <Text>{comment.text}</Text>
-      </View>
-      <View>
+      <View style={styles.bottomcontainer}>
       <TouchableWithoutFeedback onPress={onLikePressed}>
             {isLiked ? (
-              <ADIcon name="heart" size={25} color={'#e73838'} />
+              <ADIcon name="heart" size={20} color={'#e73838'} />
             ) : (
-              <ADIcon name="hearto" size={25} color={'#545454'} />
+              <ADIcon name="hearto" size={20} color={'#545454'} />
             )}
       </TouchableWithoutFeedback>
-      <Text>56 Likes</Text>
+      <Text style={styles.likes}>56 Likes</Text>
       </View>
     </View>
   );
@@ -50,21 +58,68 @@ export default CommentItem;
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
+      margin: 3,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      shadowColor:"black",
+      shadowOpacity: 0.1,
+      shadowRadius:4,
+      shadowOffset: {
+      width: -2,
+      height: 2,
+      },
+      backgroundColor: 'white',
+      opacity: 0.88,
+      borderRadius: 10,
+      flexDirection: "column",
+    },
+
+
+  topcontainer: {
+    flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    margin: 3,
+    marginHorizontal: 3,
   },
   left: {
     flexDirection: 'row',
+    flex: 1,
   },
   right: {
-    marginRight: 15,
-    marginLeft: 5,
+    flex: 5,
+  },
+  top: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex:1,
+  },
+  date: {
+    marginRight: 5,
+    fontFamily: themeFontFamily.raleway,
+  },
+  comment: {
+    flexDirection: 'column',
+    alignSelf: 'baseline',
+    fontFamily: themeFontFamily.raleway,
   },
   name: {
     alignSelf: 'center',
     fontWeight: 'bold',
     color: '#3c3c3c',
+    fontFamily: themeFontFamily.ralewayBold,
   },
+  bottomcontainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 14,
+    marginBottom:5,
+  },
+  likes: {
+    margin: 5,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontFamily: themeFontFamily.ralewayThin,
+    color: '#3c3c3c',
+  },
+
 });
